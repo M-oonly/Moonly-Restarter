b78eecf7fe1f87cee93cbc0121720e887c5a887e
M-oonly <133808297+M-oonly@users.noreply.github.com>
2023-05-17 10:38:53 -0700

    Add files via upload

---
+++ keep-alive.py
@@ -0,0 +1,33 @@
+# RESTARTER BY Revive#8798
+# WEBHOOK AND JSON SUPPORT BY Moonly#7996
+
+import requests
+import subprocess
+import json
+
+with open('moonly-config.json', 'r') as f:
+  data = json.load(f)
+
+webhook = data["webhookUrl"]
+minutes_per_restart = data["minutes"]
+
+embed_data = {
+    "color": 15315945,
+    "author": {
+        "name": "Moonly#7996 just refreshed your Mewt Sniper :)"
+    }
+}
+
+while True:
+    process = subprocess.Popen(['python', 'main.py'])
+    requests.post(webhook, json={"content": None, "embeds": [embed_data]})
+    try:
+        process.wait(minutes_per_restart * 60)
+    except KeyboardInterrupt:
+        print("Exiting")
+        break
+    except Exception:
+        pass
+    process.kill()
+    print("Restarting")
+    continue
